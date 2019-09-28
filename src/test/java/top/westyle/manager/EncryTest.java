05e736e6da04868d902c166fba726664d8502f22
yanjunmin <1519484516@qq.com>
2019-09-28 10:08:09 +0800

    update

---
+++ src/test/java/top/westyle/manager/EncryTest.java
@@ -0,0 +1,17 @@
+package top.westyle.manager;
+
+import com.cxytiandi.encrypt.util.AesEncryptUtils;
+
+/**
+ * 测试加密解密
+ */
+public class EncryTest {
+    public static void main(String[] args) throws Exception {
+        String content = "{\"userName\":\"yanjdfsddugfgns0\", \"password\":\"1234560\"}";
+        System.out.println("加密前：" + content);
+        String encrypt = AesEncryptUtils.aesEncrypt(content, "abcdefyjm3456789");
+        System.out.println(encrypt.length() + ":加密后：" + encrypt);
+        String decrypt = AesEncryptUtils.aesDecrypt(encrypt, "abcdefyjm3456789");
+        System.out.println("解密后：" + decrypt);
+    }
+}
