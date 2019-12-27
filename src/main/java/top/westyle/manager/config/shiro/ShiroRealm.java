7ce66193c58baa24c34f6488cf4c7848c32fc8a2
company <1519484516@qq.com>
2019-12-27 18:00:03 +0800

    update

--- src/main/java/top/westyle/manager/config/shiro/ShiroRealm.java
+++ src/main/java/top/westyle/manager/config/shiro/ShiroRealm.java
@@ -1,16 +1,23 @@
 package top.westyle.manager.config.shiro;
 
+import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
 import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import top.westyle.manager.entity.common.User;
 import top.westyle.manager.service.UserService;
+
+import java.util.Collection;
 
 /**
  * @description 自定义shiro的realm实现
@@ -50,6 +57,16 @@ public class ShiroRealm extends AuthorizingRealm {
         log.debug("登录验证");
         UsernamePasswordToken upToken = (UsernamePasswordToken)authenticationToken;
         String loginName = upToken.getUsername();
+        DefaultWebSecurityManager defaultWebSecurityManager = (DefaultWebSecurityManager)SecurityUtils.getSecurityManager();
+        ShiroRedisSessionManager shiroRedisSessionManager = (ShiroRedisSessionManager)defaultWebSecurityManager.getSessionManager();
+        Collection<Session> sessions = shiroRedisSessionManager.getSessionDAO().getActiveSessions();//获取当前已登录用户的sesion列表
+        for (Session session : sessions) {
+            log.info("session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY)){}", session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY));
+            if(loginName.equals(String.valueOf(session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY)))) {
+                //删除用户之前登录的session
+                shiroRedisSessionManager.getSessionDAO().delete(session);
+            }
+        }
         //进行用户名信息查询
         User user  = userService.findByUserName(loginName);
         if(user == null) {
